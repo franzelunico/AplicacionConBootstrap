e11a1af8f89ed66eafa7b390a46aae03ec7cec39
Franz Lopez <franzelunico@gmail.com>
2020-09-27 19:10:11 -0400

    Automatizadores de tareas Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 'use strict';
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function () {
         gulp.src('./css/*.scss')
@@ -14,7 +22,7 @@ gulp.task('sass:watch', function () {
 });
 
 gulp.task('browser-sync', function () {
-    var files = ['./*.html', './css/*.css', './img/*.{png, jpg, gif, webp}', './js/*.js']
+    var files = ['./*.html', './css/*.css', './images/*.{png, jpg, gif, webp}', './js/*.js']
     browserSync.init(files, {
         server: {
             baseDir: './'
@@ -23,4 +31,39 @@ gulp.task('browser-sync', function () {
 });
 gulp.task('default', ['browser-sync'], function () {
     gulp.start('sass:watch');
+});
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function () {
+    gulp.src('node_modules/open-iconic/font/fonts/*.{ttf,woff,eof,svg,eot,otf}*')
+        .pipe(gulp.dest('./dist/fonts'))
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('./images/*.{jpg,png,webp}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function (stream, file) {
+            return stream
+                .pipe(usemin({
+                    css: [rev()],
+                    html: [function () { return htmlmin({ collapseWhitespace: true }) }],
+                    js: [uglify(), rev()],
+                    inlinejs: [uglify()],
+                    inlinecss: [cleanCss(), 'concat']
+                }));
+        }))
+        .pipe(gulp.dest('dist/'));
+    //flat map allows us to process in paralel
+});
+
+gulp.task('build', ['clean'], function () {
+    gulp.start('copyfonts','imagemin', 'usemin')
 });
